6671cfedc9828add72d6301b257ea22f92846c0f
Brian Clifton <briancliftonstudio@gmail.com>
2015-06-12 14:52:14 -0400

    init

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	console.log($("#t-widget").width());
+
+
+	$('img.autosized-media').css('width', '600px');
+
+		// width: 600px;
+		// height: 294px;
+		// margin-bottom: 5px;
+});
